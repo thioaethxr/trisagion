aaebe0bee61e5e333aee3396c7a6b9354d00537a
thioaethxr <thioaethxr@proton.me>
2023-12-04 21:29:47 +0100

    FE: Creating the 404 page and adding title prop to the Default layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { HomePage } from '@pages/HomePage';
+import { NotFoundPage } from '@pages/NotFoundPage';
 
 import { AppRoute } from '@utils/route';
 
 export const App: React.FC = () => (
   <Routes>
-    <Route element={<HomePage />} path={AppRoute.HOME} />
+    <Route path={AppRoute.HOME} element={<HomePage />} />
+    <Route path={AppRoute.NOT_FOUND} element={<NotFoundPage />} />
   </Routes>
 );
--- client/src/utils/route.ts
+++ client/src/utils/route.ts
@@ -1,6 +1,10 @@
+export const PAGE_TITLE_BASE = 'Trisagion';
+export const PAGE_TITLE_SPLITTER = ' | ';
+
 /**
  * Enumeration representing application routes.
  */
 export enum AppRoute {
   HOME = '/',
+  NOT_FOUND = '*',
 }
