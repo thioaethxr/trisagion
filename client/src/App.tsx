ef93e1a02ff22c05c0521eb20cc95e7105563f53
thioaethxr <thioaethxr@proton.me>
2023-12-04 21:05:22 +0100

    FE: Setting up import aliases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { HomePage } from './pages/HomePage';
+import { HomePage } from '@pages/HomePage';
 
-import { AppRoute } from './utils/route';
+import { AppRoute } from '@utils/route';
 
 export const App: React.FC = () => (
   <Routes>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 });
